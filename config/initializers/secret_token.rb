ab6542ccc7738d92e03a1b69342a680549fefa69
Rafael Scarenci <scarenci@gmail.com>
2012-07-23 20:00:27 -0300

    start project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Completaotanque::Application.config.secret_token = '399adef375687e236b663c73a5713ce52dcbd2c1e2ff6f2f9ff2cc81fdb0862c26981f9832ffff660104e2ea7ea58e56320e1a8d4751f0395a09317de31fc8f6'
